82eca7c36f8dd2654cff8a31d85ee05f361b7196
fabCampani <campa_fab@hotmail.it>
2013-12-17 19:52:05 +0100

    Aggiunti File

---
+++ ProgettoMMDS/GreedyAntsScheduler.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgettoMMDS
+{
+    class GreedyAntsScheduler : AbstractScheduler
+    {
+        List<Job> jobs = new List<Job>();
+        volatile static bool fine = false;
+        static long MTIME = 1000;
+        Schedule schedule;
+
+        //Matrice della traccia di ferormoni:
+        double[,] traceMatrix;
+
+        public override void run(string[] args)
+        {
+            if (!(args.Length > 0))
+            {
+                Console.WriteLine("PATH non specificato");
+                Console.ReadKey();
+                return;
+            }
+            else
+            {
+                FileManager fm = new FileManager(args[0]);
+                jobs = (fm.getJobsList());
+                //INIZIO CONTEGGIO SECONDI
+                DateTime startTime = DateTime.Now;
+                //QUI l'algoritmo di ricerca del minimo ->
+                Schedule schedule = greedyAnts(fm.getNumberofMachine(), fm.getNumberofJobs());
+
+                //FINE CONTEGGIO SECONDI
+                DateTime stopTime = DateTime.Now;
+                TimeSpan elapsedTime = stopTime.Subtract(startTime);
+                //Stampa Tardiness e tempo totale
+                Console.WriteLine("Tardiness totale: " + schedule.getTardiness().ToString());
+                Console.WriteLine("Arrivato in " + elapsedTime.TotalMilliseconds + " ms");
+
+                //OUTPUT
+                fm.OutputSolution(schedule.schedule);
+                fm.OutputResult(schedule.getTardiness(), elapsedTime.TotalMilliseconds);
+                fm.OutputProva(schedule.schedule, schedule.getTardiness(), elapsedTime.TotalMilliseconds, "Prova");
+                Console.ReadKey();
+            }
+        }
+
+        private Schedule greedyAnts(int m, int n)
+        {
+            schedule = new Schedule(jobs);
+            schedule.constructScheduleEDD(m, n);
+            //Inizializzazione matrice delle traccie
+            traceMatrix = new double[schedule.Count(), schedule.Count()];
+            for (int i = 0; i < schedule.Count(); i++)
+            {                
+                for (int j = 0; j < schedule.Count(); j++)
+                {
+                    traceMatrix[i, j] = 50;
+                }
+            }
+            int iteration = 0;
+            while (!fine)
+            {
+                constructGreedysolutions(iteration);
+            }
+            return schedule;
+        }
+
+        private void constructGreedysolutions(int i)
+        {
+            //NUMERO DI FORMICHE
+            int ants = 10;
+            Stack<Schedule> solutionList = new Stack<Schedule>();
+            //faccio partire le formiche
+            for (int j = 0; j < ants; j++)
+            {
+                solutionList.Push(antRun(i));
+            }
+            int bestTardiness = schedule.getTardiness();
+            
+            while (solutionList.Count != 0)
+            {
+                Schedule currentSchedule = solutionList.Pop();
+                if (currentSchedule.getTardiness() < bestTardiness)
+                {
+                    schedule = new Schedule(currentSchedule);
+                    int currentTardiness = currentSchedule.getTardiness();
+                    if (currentTardiness < bestTardiness)
+                    {
+                        schedule = new Schedule(currentSchedule);
+                        bestTardiness = currentTardiness;
+                    }
+                }
+            }            
+        }
+
+        private Schedule antRun(int i)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ProgettoMMDS/Schedule.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgettoMMDS
+{
+    public class Schedule
+    {
+        List<int> _schedule;
+        public List<int> schedule
+        {
+            get
+            {
+                return _schedule;
+            }
+            set
+            {
+                _schedule = new List<int>(value);
+            }
+        }
+
+        List<Job> _jobs;
+        public List<Job> jobs
+        {
+            get
+            {
+                return _jobs;
+            }
+        }
+
+        public Schedule(List<Job> arg1)
+        {
+            _jobs = new List<Job>(arg1);
+            _schedule = null;
+        }
+        public Schedule(Schedule scheduleToCopy)
+        {
+            _schedule = new List<int>(scheduleToCopy.schedule);
+            _jobs = scheduleToCopy.jobs;
+        }
+
+        /// <summary>
+        /// Costruisce uno Schedule EDD prendendo in input macchine e la lista dei job  
+        /// </summary>
+        /// <param name="m">Numero Macchine</param>
+        /// <param name="n">Numero Job</param>
+        /// <returns></returns>
+        public void constructScheduleEDD(int m, int n)
+        {
+            //Ho creato qui una nuova lista temporanea -> la lista jobs non viene toccata.
+            List<Job> tempJobs = new List<Job>(jobs);
+            //Ordinamento qui -> In ordine di DueDate:
+            tempJobs.Sort((x, y) => x.getDueDateTime().CompareTo(y.getDueDateTime()));
+            //a questo punto tempJobs è ordinato in base alla DueDate...
+
+            _schedule = new List<int>();
+            int[] time = new int[m];
+            int[] index = new int[m];
+            for (int i = 0; i < m; i++)
+            {
+                index[i] = i;
+                time[i] = 0;
+            }
+            for (int i = 0; i < m - 1; i++)
+            {
+                _schedule.Add(0);
+            }
+            int bestMac = 0;
+            for (int i = 0; i < n; i++)
+            {
+                Job j = jobs[i];
+
+                _schedule.Insert(index[bestMac], j.getID());
+                for (int k = bestMac; k < m; k++)
+                    index[k]++;
+                time[bestMac] += j.getProcessingTime();
+                for (int k = 0; k < m; k++)
+                {
+                    if (time[k] < time[bestMac])
+                    {
+                        bestMac = k;
+                    }
+                }
+            } 
+        }
+
+        /// <summary>
+        /// Funzione che calcola la Tardiness totale di uno schedule dato
+        /// </summary>
+        /// <returns>Tardiness totale</returns>
+        public int getTardiness()
+        {
+            int time = 0;
+            int tardiness = 0;
+
+            foreach (int i in _schedule)
+            {
+                if (i == 0)
+                    time = 0;
+                else
+                {
+                    Job j = _jobs[i - 1];
+                    time += j.getProcessingTime();
+                    if (time > j.getDueDateTime())
+                    {
+                        tardiness += (time - j.getDueDateTime());
+                    }
+                }
+            }
+            return (tardiness);
+        }
+        /// <summary>
+        /// Restituisce il numero degli elementi presenti nello schedule
+        /// Comprese le macchine
+        /// </summary>
+        /// <returns>numero degli elementi</returns>
+        public int Count()
+        {
+            return _schedule.Count();
+        }
+
+        public void swap(int num1, int num2){
+            int temp = _schedule[num1];
+            _schedule[num1] = _schedule[num2];
+            _schedule[num2] = temp;
+        }
+    }
+}
